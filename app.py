a8424f5f394f9fd3bb0d861269ca1edcced5a62f
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-07-18 12:52:00 +0000

    add random_cafe and all_cafes get route methods, add to_dict method to cafe class to return table column name as keys and get their attributes as values

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from flask import Flask
+from random import choice
+
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String
+from sqlalchemy import String, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_restful import Resource, Api
 
 
 class Base(DeclarativeBase):
@@ -24,12 +27,32 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool]
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self) -> dict:
+        return {
+            column.name: getattr(self, column.name) for column in self.__table__.columns
+        }
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db.init_app(app)
+api = Api(app, catch_all_404s=True)
 
 
 @app.route('/random')
 def random_cafe():
-    return
+    result = db.session.execute(select(Cafe))
+    all_cafes = result.scalars().all()
+    rand_cafe = choice(all_cafes)
+    return jsonify(cafe=rand_cafe.to_dict())
+
+
+@app.route('/all-cafes')
+def all_cafes():
+    result = db.session.execute(select(Cafe))
+    all_cafes = result.scalars().all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
